eefbca312bb01c4afdef71bcdda9be17c283a9ec
Patrick Jennings <pkjennings999@gmail.com>
2017-11-26 01:36:22 +0000

    Committing framework

---
+++ src/TEMP_NAME.java
@@ -0,0 +1,26 @@
+import processing.core.PApplet;
+
+public class TEMP_NAME extends PApplet
+{
+
+	public static void main(String[] args)
+	{
+		PApplet.main("TEMP_NAME");
+	}
+
+	public void settings()
+	{
+		size(800, 600);
+	}
+
+	public void setup()
+	{
+
+	}
+
+	public void draw()
+	{
+
+	}
+
+}
