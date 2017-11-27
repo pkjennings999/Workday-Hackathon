997bd0608e373748339c7a23a9325b62d50d4961
Patrick Jennings <pkjennings999@gmail.com>
2017-11-27 14:13:46 +0000

    New Package

---
+++ src/Praptrick/TEMP_NAME.java
@@ -0,0 +1,28 @@
+package Praptrick;
+
+import processing.core.PApplet;
+
+public class TEMP_NAME extends PApplet
+{
+
+	public static void main(String[] args)
+	{
+		PApplet.main("TEMP_NAME");
+	}
+
+	public void settings()
+	{
+		size(900, 600);
+	}
+
+	public void setup()
+	{
+
+	}
+
+	public void draw()
+	{
+
+	}
+
+}
