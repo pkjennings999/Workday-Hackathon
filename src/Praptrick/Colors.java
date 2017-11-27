cbe4a2252a35a70e6e9b1827724120f185a4f1a3
Patrick Jennings <pkjennings999@gmail.com>
2017-11-27 15:32:24 +0000

    Tidying

--- src/Praptrick/Colors.java
+++ src/Praptrick/Colors.java
@@ -2,14 +2,13 @@ package Praptrick;
 
 import java.awt.*;
 
-    public class Colors {
-        public static final Color DARK_GREEN = new Color(0xD1E7FF);
-        public static final Color PICTON_BLUE = new Color(0x2F6AFE);
-        public static final Color AZUREISH_WHITE = new Color(0xFFFDF5);
-        public static final Color DEEP_KOMARU = new Color(0x760082);
-        public static final Color ASH_GREY = new Color(0xB22610);
-        public static final Color MAP_COLOR = new Color(0x5ddde0);
-        public static final Color DATE_INDICATOR_COLOR = new Color(0xFF00FF);
-    }
-
-
+public class Colors
+{
+	public static final Color	DARK_GREEN				= new Color(0xD1E7FF);
+	public static final Color	PICTON_BLUE				= new Color(0x2F6AFE);
+	public static final Color	AZUREISH_WHITE			= new Color(0xFFFDF5);
+	public static final Color	DEEP_KOMARU				= new Color(0x760082);
+	public static final Color	ASH_GREY				= new Color(0xB22610);
+	public static final Color	MAP_COLOR				= new Color(0x5ddde0);
+	public static final Color	DATE_INDICATOR_COLOR	= new Color(0xFF00FF);
+}
--- src/Praptrick/GlobalVariables.java
+++ src/Praptrick/GlobalVariables.java
@@ -1,18 +1,17 @@
 package Praptrick;
+
 public class GlobalVariables
 {
-	public static boolean		homescreen			= true;
-	public static boolean		createRequestScreen	= false;
-	public static boolean		createScreen		= false;
-	public static boolean		requestScreen		= false;
-	
-	
-	public static String playListNameString ="";
-	public static String passwordString = "";
-	public static String requestSongString = "";
-	public static String playListOut;
-	public static String passwordStringOut;
-	public static String requestNameOut;
-	
+	public static boolean	homescreen			= true;
+	public static boolean	createRequestScreen	= false;
+	public static boolean	createScreen		= false;
+	public static boolean	requestScreen		= false;
+
+	public static String	playListNameString	= "";
+	public static String	passwordString		= "";
+	public static String	requestSongString	= "";
+	public static String	playListOut;
+	public static String	passwordStringOut;
+	public static String	requestNameOut;
 
 }
